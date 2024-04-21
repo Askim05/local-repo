7c0587ee5a1089c8ebd8f6fd44dfa7c9f4ab1653
Askim05 <zraaaskim@gmail.com>
2024-04-21 16:35:16 +0530

    conf

--- pullReq.java
+++ pullReq.java
@@ -1,12 +1,13 @@
 class pullReq{
 
 
-    public static Merging(){
-        System.out.println("In feature "+n);
-
+    public static Merging(int n){
+        int x,y,z;
+        //System.out.println(n);
     }
 
     public static void main(String args[]){
+        Merging(3);
         System.out.println("Conflict");
     }
 
