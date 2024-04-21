0f68deb84d41ff53a323845f211bf01238c55799
Askim05 <zraaaskim@gmail.com>
2024-04-21 18:39:28 +0530

    merge from feat

--- pullReq.java
+++ pullReq.java
@@ -6,6 +6,9 @@ class pullReq{
         System.out.println(n);
 
     }
+    public static branchChange(){
+        System.out.println("Merging from branch");
+    }
     public static void main(String args[]){
         merging("3");
         System.out.println("Conflict");
