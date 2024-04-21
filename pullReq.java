76767d2d7d7050409b397bca78055d6b3a6084ca
Askim05 <zraaaskim@gmail.com>
2024-04-21 18:19:12 +0530

    merge

--- pullReq.java
+++ pullReq.java
@@ -2,12 +2,12 @@ class pullReq{
 
 
     public static Merging(string n){
-        System.out.println("Conf");
+        //System.out.println("Conf");
         //System.out.println(n);
     }
 
     public static void main(String args[]){
-        Merging("3");
+        Merging("");
         System.out.println("Conflict");
     }
 
