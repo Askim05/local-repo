4f722b4a62933cc6b4db60d6f13d3270b8de4d6f
Askim05 <zraaaskim@gmail.com>
2024-04-21 15:52:13 +0530

    Add merging

--- pullReq.java
+++ pullReq.java
@@ -1,5 +1,16 @@
 class pullReq{
+
+
+    public static Merging(int n){
+        System.out.println("In feature "+n);
+
+    }
+
     public static void main(String args[]){
+
+        Merging(3);
         System.out.println("Hi..!!!");
     }
+
+
 }
